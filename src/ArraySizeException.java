59c81ca0fdc10d1a771a503067a593e79fdcbfb2
Vladakkk <vladakasperskaa@gmail.com>
2024-04-23 20:40:58 +0300

    exceptions

---
+++ src/ArraySizeException.java
@@ -0,0 +1,6 @@
+public class ArraySizeException extends RuntimeException{
+
+    public ArraySizeException(String message) {
+        super(message);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,46 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String[][] array1 = {
+                {"1", "4", "2", "5"},
+                {"3", "6", "9", "7"},
+                {"8", "10", "12", "14"},
+                {"11", "13", "16", "15"}
+        };
+
+        String[][] array2 = {
+                {"10", "2", "31", "55"},
+                {"15", "12", "27", "81"},
+                {"9", "1", "41", "12"},
+                {"13", "34", "25", "16", "2"}
+        };
+
+        String[][] array3 = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "apple", "16"}
+        };
+
+        try {
+            int result = ArrayValueCalculator.doCalc(array1);
+            System.out.println("Calculation result for array 1: " + result);
+        } catch (ArraySizeException | ArrayDataException exception){
+            System.out.println("Calculation error for array 1: " + exception.getMessage());
+        }
+
+        try {
+            int result = ArrayValueCalculator.doCalc(array2);
+            System.out.println("Calculation result for array 2: " + result);
+        } catch (ArraySizeException | ArrayDataException exception){
+            System.out.println("Calculation error for array 2: " + exception.getMessage());
+        }
+
+        try {
+            int result = ArrayValueCalculator.doCalc(array3);
+            System.out.println("Calculation result for array 3: " + result);
+        } catch (ArraySizeException | ArrayDataException exception){
+            System.out.println("Calculation error for array 3: " + exception.getMessage());
+        }
     }
 }
